55d0dcd85710906ac79b3063bc1a60adbcac379d
phkh-tech <longphkh@gmail.com>
2026-05-18 20:23:42 +0700

    Add Blueprint GA and GC

--- Combat_PHKH/Source/Combat_PHKH/Private/GA/Attributes/BaseAttributeSet.cpp
+++ Combat_PHKH/Source/Combat_PHKH/Private/GA/Attributes/BaseAttributeSet.cpp
@@ -5,7 +5,10 @@
 
 UBaseAttributeSet::UBaseAttributeSet()
 {
-	
+	Health = 100.f;
+ 	MaxHealth = 100.f;
+	Stamina = 100.f;
+	MaxStamina = 100.f;
 }
 
 void UBaseAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
