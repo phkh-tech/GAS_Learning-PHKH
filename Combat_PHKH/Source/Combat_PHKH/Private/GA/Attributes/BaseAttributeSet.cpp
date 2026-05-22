bc04fdf59104ab50535bde0903b40301b12895f9
phkh-tech <longphkh@gmail.com>
2026-05-22 09:16:29 +0700

    Adding GameEffect

--- Combat_PHKH/Source/Combat_PHKH/Private/GA/Attributes/BaseAttributeSet.cpp
+++ Combat_PHKH/Source/Combat_PHKH/Private/GA/Attributes/BaseAttributeSet.cpp
@@ -2,6 +2,7 @@
 
 
 #include "GA/Attributes/BaseAttributeSet.h"
+#include "Net/UnrealNetwork.h"
 
 UBaseAttributeSet::UBaseAttributeSet()
 {
@@ -14,4 +15,9 @@ UBaseAttributeSet::UBaseAttributeSet()
 void UBaseAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
 {
 	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	
+	DOREPLIFETIME_CONDITION_NOTIFY(UBaseAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UBaseAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UBaseAttributeSet, Stamina, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UBaseAttributeSet, MaxStamina, COND_None, REPNOTIFY_Always);
 }
